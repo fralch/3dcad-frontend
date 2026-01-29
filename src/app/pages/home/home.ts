ff4b13ae3ec060a0f296d3d52cae7f960a1775c9
Frank Cairampoma <ingfralch@gmail.com>
2026-01-29 17:08:04 -0500

    feat: Implement initial application layout including header, footer, and a comprehensive home page UI.

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { CATEGORIES } from '../../core/constants/categories';
 
 @Component({
   selector: 'app-home',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
